33234ff82766a510b232a2a932cc5c022ab4bc2b
Tomás Mena <tomasmmena@gmail.com>
2023-12-17 16:54:27 -0300

    Second part solved.

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,18 @@ fn get_next_in_sequence(sequence: &Vec<i64>) -> i64 {
     }
 }
 
+fn get_previous_in_sequence(sequence: &Vec<i64>) -> i64 {
+    let diff_sequence: Vec<i64> = sequence
+        .windows(2)
+        .map(|w| w[1] - w[0])
+        .collect();
+    if diff_sequence.iter().all(|n| *n == 0) {
+        *sequence.first().expect("Sequence is empty!")
+    } else {
+        sequence.first().expect("Sequence is empty!") - get_previous_in_sequence(&diff_sequence)
+    }
+}
+
 fn main() {
     let path = env::args().nth(1).expect("Missing required parameter path!");
 
@@ -29,7 +41,7 @@ fn main() {
 
     let mut sum_new_values: i64 = 0;
     for sequence in data.iter() {
-        sum_new_values += get_next_in_sequence(sequence);
+        sum_new_values += get_previous_in_sequence(sequence);
     }
 
     println!("Sum of new values: {}", sum_new_values);
@@ -45,4 +57,17 @@ fn test_get_next_in_sequence() {
 
     let sequence_3: Vec<i64> = vec![10, 13, 16, 21, 30, 45];
     assert_eq!(get_next_in_sequence(&sequence_3), 68);
+}
+
+
+#[test]
+fn test_get_previous_in_sequence() {
+    let sequence_1: Vec<i64> = vec![0, 3, 6, 9, 12, 15];
+    assert_eq!(get_previous_in_sequence(&sequence_1), -3);
+
+    let sequence_2: Vec<i64> = vec![1, 3, 6, 10, 15, 21];
+    assert_eq!(get_previous_in_sequence(&sequence_2), 0);
+
+    let sequence_3: Vec<i64> = vec![10, 13, 16, 21, 30, 45];
+    assert_eq!(get_previous_in_sequence(&sequence_3), 5);
 }
